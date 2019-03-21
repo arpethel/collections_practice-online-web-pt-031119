295fb887cd37909f1efbeba01a2c11f15f192096
Ryan Pethel <arpethel@gmail.com>
2019-03-21 22:05:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,7 +41,9 @@ def sum_array(array)
   array.inject { |sum, n| sum + n}
 end
 
-
+def add_s(array)
+  array.each_with_index.collect { |ele, idx| ele + 's' unless ele == array[1] end}
+end
 
 
 # end
